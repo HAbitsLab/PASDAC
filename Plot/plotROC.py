7dbe5d120d09439f0efafe77d2d1d56cbfce560e
rawanalharbi <harbi.rawan@gmail.com>
2020-01-21 11:43:21 -0600

    updated ROC plot

--- Plot/plotROC.py
+++ Plot/plotROC.py
@@ -4,8 +4,15 @@ from itertools import cycle
 from sklearn.preprocessing import label_binarize
 from sklearn.metrics import roc_curve, auc
 from scipy import interp
+import colorsys
 
-def plotROC(labels, probas, n_classes):
+
+def make_colors(num):
+    for i in np.arange(0.0, 360.0, 360.0 / num):
+        yield colorsys.hls_to_rgb(i / 360.0, 0.7, 0.9)
+
+
+def plotROC(labels, probas, n_classes, savepath=None):
     """Plot all ROC curves
 
     Parameters
@@ -13,18 +20,19 @@ def plotROC(labels, probas, n_classes):
     labels:             numpy array
     probas:             numpy array
     n_classes:          int
-    
+
     Return
     ------
-    
+
     """
 
     # labels starts from 0
     if n_classes == 2:
-        labels = np.hstack([max(np.unique(labels)) - labels,labels-min(np.unique(labels))])
+        label_col0 = (max(np.unique(labels)) - labels)[np.newaxis].T
+        label_col1 = (labels - min(np.unique(labels)))[np.newaxis].T
+        labels = np.hstack([label_col0, label_col1])
     else:
         labels = label_binarize(labels, classes=[i for i in range(n_classes)])
-
 
     # Compute ROC curve and ROC area for each class
     fpr = dict()
@@ -34,42 +42,13 @@ def plotROC(labels, probas, n_classes):
         fpr[i], tpr[i], _ = roc_curve(labels[:, i], probas[:, i])
         roc_auc[i] = auc(fpr[i], tpr[i])
 
-    # Compute micro-average ROC curve and ROC area
-    fpr["micro"], tpr["micro"], _ = roc_curve(labels.ravel(), probas.ravel())
-    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
-
-    # First aggregate all false positive rates
-    all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
-
-    # Then interpolate all ROC curves at this points
-    mean_tpr = np.zeros_like(all_fpr)
-    for i in range(n_classes):
-        mean_tpr += interp(all_fpr, fpr[i], tpr[i])
-
-    # Finally average it and compute AUC
-    mean_tpr /= n_classes
-
-    fpr["macro"] = all_fpr
-    tpr["macro"] = mean_tpr
-    roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
-
     # Plot all ROC curves
     plt.figure()
-    plt.plot(fpr["micro"], tpr["micro"],
-             label='micro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["micro"]),
-             color='deeppink', linestyle=':', linewidth=4)
-
-    plt.plot(fpr["macro"], tpr["macro"],
-             label='macro-average ROC curve (area = {0:0.2f})'
-                   ''.format(roc_auc["macro"]),
-             color='navy', linestyle=':', linewidth=4)
-
-    colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+    colors = list(make_colors(n_classes))
     for i, color in zip(range(n_classes), colors):
         plt.plot(fpr[i], tpr[i], color=color, lw=2,
                  label='ROC curve of class {0} (area = {1:0.2f})'
-                 ''.format(i, roc_auc[i]))
+                       ''.format(i, roc_auc[i]))
 
     plt.plot([0, 1], [0, 1], 'k--', lw=2)
     plt.xlim([0.0, 1.0])
@@ -78,9 +57,11 @@ def plotROC(labels, probas, n_classes):
     plt.ylabel('True Positive Rate')
     plt.title('Receiver Operating Characteristic')
     plt.legend(loc="lower right")
-    plt.show()
 
-    return
+    if savepath:
+        plt.savefig(savepath, bbox_inches='tight')
+
+    plt.show()
 
 
 if __name__ == "__main__":
